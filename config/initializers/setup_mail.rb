c5bdfbe163cc187b766cd20dbf9cc4c3bb9a381a
Jordan Epps <jordan.k.epps@gmail.com>
2015-10-23 10:15:45 -0700

    Mailer bug fixes.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,7 +5,7 @@ if Rails.env.development? || Rails.env.production?
 		port: 					'2525',
 		authentication: :plain,
 		user_name: 			ENV['SENDGRID_USERNAME'],
-		password: 			ENV['SENGRID_PASSWORD'],
+		password: 			ENV['SENDGRID_PASSWORD'],
 		domain: 				'heroku.com',
 		enable_starttls_auto: true
 	}
